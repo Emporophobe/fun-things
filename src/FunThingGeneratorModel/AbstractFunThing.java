73e6196bfe0659d6efc65898300dac3c64dadf65
lbreite <lbreite@ccs.neu.edu>
2016-10-01 14:50:08 -0400

    Fixed IFunThing

--- src/FunThingGeneratorModel/AbstractFunThing.java
+++ src/FunThingGeneratorModel/AbstractFunThing.java
@@ -1,5 +1,7 @@
 package FunThingGeneratorModel;
 
+import java.util.List;
+
 /**
  * Fun things!
  */
@@ -17,4 +19,22 @@ public abstract class AbstractFunThing implements IFunThing {
     }
 
     public abstract String getFormattedInformationString();
+
+    /**
+     * Get a list of activities that fit the given parameters.
+     *
+     * @param participants The number of participants
+     * @param minMinutes   The minimum length of time to do stuff in minutes
+     * @param maxMinutes   The maximum length of time to do stuff in minutes
+     * @param minCost      The minimum cost of the activity in dollars.
+     * @param maxCost      The maximum cost of the activity in dollars.
+     * @param isOutside    Whether you have to leave the house to do the thing.
+     * @return A list of IFunThing that fit the requirements.
+     */
+    abstract List<IFunThing> getListOfFunThings(int participants,
+                                       int minMinutes,
+                                       int maxMinutes,
+                                       int minCost,
+                                       int maxCost,
+                                       boolean isOutside);
 }
