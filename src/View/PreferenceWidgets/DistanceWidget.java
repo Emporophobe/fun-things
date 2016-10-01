7bdd1c9a26964c1839cafda964a55eee92f062d9
DJ Richardson <dj@neuroscouting.com>
2016-10-01 16:40:08 -0400

    Fleshed out widgets for the preference screen.

--- src/View/PreferenceWidgets/DistanceWidget.java
+++ src/View/PreferenceWidgets/DistanceWidget.java
@@ -7,4 +7,33 @@ import java.awt.*;
  * Created by DJ on 10/1/2016.
  */
 public class DistanceWidget extends JPanel {
+    private JSlider slider;
+    private JLabel minLabel;
+    private JLabel maxLabel;
+
+    public DistanceWidget() {
+        GridBagLayout layout = new GridBagLayout();
+        this.setLayout(layout);
+        minLabel = new JLabel("Min (km)");
+        maxLabel = new JLabel("Max (km)");
+        slider = new JSlider(0, 5);
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 3;
+        constraints.gridheight = 1;
+        this.add(slider, constraints);
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+        this.add(minLabel, constraints);
+        constraints.gridx = 2;
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        constraints.gridheight = 1;
+        constraints.anchor = GridBagConstraints.EAST;
+        this.add(maxLabel, constraints);
+
+    }
 }
--- src/View/PreferenceWidgets/TimeRangeWidget.java
+++ src/View/PreferenceWidgets/TimeRangeWidget.java
@@ -7,4 +7,13 @@ import java.awt.*;
  * Created by DJ on 10/1/2016.
  */
 public class TimeRangeWidget extends JPanel {
+    private int time = 10;
+    JSpinner minuteSpinner;
+
+    public TimeRangeWidget() {
+        minuteSpinner = new JSpinner(new SpinnerNumberModel(time, 0, 640, 10));
+        JLabel row = new JLabel(" in minutes");
+        this.add(minuteSpinner);
+        this.add(row);
+    }
 }
