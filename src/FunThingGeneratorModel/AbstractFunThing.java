cf0086690151026b1fe01864cf7fa7d33f84fbca
Michael <emporophobe@gmail.com>
2016-10-01 13:53:52 -0400

    Rearchitectured the FunThing interface and abstract base class.

--- src/FunThingGeneratorModel/AbstractFunThing.java
+++ src/FunThingGeneratorModel/AbstractFunThing.java
@@ -4,59 +4,8 @@ package FunThingGeneratorModel;
  * Fun things!
  */
 public abstract class AbstractFunThing implements IFunThing {
-
-    Category theCategory;
-    String theName;
-    int theRating;
-    int maxRating;
-
-    int minPeople;
-    int maxPeople;
-    int minMinutes;
-    int maxMinutes;
-    int minCost;
-    int maxCost;
-    int distance;
-
-    @Override
-    public double getDistance() {
-        return distance;
-    }
-
-    @Override
-    public int getMinPeople() {
-        return minPeople;
-    }
-
-    @Override
-    public int getMaxPeople() {
-        return maxPeople;
-    }
-
-    @Override
-    public int getMinMinutes() {
-        return minMinutes;
-    }
-
-    @Override
-    public int getMaxMinutes() {
-        return maxMinutes;
-    }
-
-    @Override
-    public int getMinCost() {
-        return minCost;
-    }
-
-    @Override
-    public int getMaxCost() {
-        return maxCost;
-    }
-
-    @Override
-    public Category getType() {
-        return theCategory;
-    }
+    protected String theName;
+    protected String details;
 
     @Override
     public String getName() {
@@ -64,7 +13,7 @@ public abstract class AbstractFunThing implements IFunThing {
     }
 
     @Override
-    public double getNormalizedRating() {
-        return theRating / maxRating;
+    public String getFormattedInformationString() {
+        return details;
     }
 }
--- src/FunThingGeneratorModel/IFunThing.java
+++ src/FunThingGeneratorModel/IFunThing.java
@@ -1,64 +1,11 @@
 package FunThingGeneratorModel;
 
+import java.util.List;
+
 /**
  * Interface for fun things.
  */
 public interface IFunThing {
-    /**
-     * Get distance to the location of the thing.
-     *
-     * @return distance in miles.
-     */
-    double getDistance();
-
-    /**
-     * Get the minimum number of participants
-     *
-     * @return The number of people
-     */
-    int getMinPeople();
-
-    /**
-     * Get the maximum number of participants
-     *
-     * @return The number of people
-     */
-    int getMaxPeople();
-
-    /**
-     * Get the minimum amount of time the thing takes.
-     *
-     * @return The minimum time in minutes
-     */
-    int getMinMinutes();
-
-    /**
-     * Get the maximum amount of time the thing takes.
-     *
-     * @return The maximum time in minutes
-     */
-    int getMaxMinutes();
-
-    /**
-     * Get the minimum cost of the thing.
-     *
-     * @return The minimum cost in dollars.
-     */
-    int getMinCost();
-
-    /**
-     * Get the maximum cost of the thing.
-     *
-     * @return The maximum cost in dollars.
-     */
-    int getMaxCost();
-
-    /**
-     * Get the {@link Category} of the thing.
-     *
-     * @return The type of the thing.
-     */
-    Category getType();
 
     /**
      * Get the name of the thing.
@@ -68,9 +15,27 @@ public interface IFunThing {
     String getName();
 
     /**
-     * Get the rating of the thing, in [0, 1]
-     *
-     * @return The rating on a scale of 0 to 1.
+     * This is a catch-all for anything you might want to show the user
+     * e.g Address, genre, etc.
+     * @return A string with miscellaneous relevant information
      */
-    double getNormalizedRating();
+    String getFormattedInformationString();
+
+    /**
+     * Get a list of activities that fit the given parameters.
+     *
+     * @param participants The number of participants
+     * @param minMinutes   The minimum length of time to do stuff in minutes
+     * @param maxMinutes   The maximum length of time to do stuff in minutes
+     * @param minCost      The minimum cost of the activity in dollars.
+     * @param maxCost      The maximum cost of the activity in dollars.
+     * @param isOutside    Whether you have to leave the house to do the thing.
+     * @return A list of IFunThing that fit the requirements.
+     */
+    List<IFunThing> getListofFunThings(int participants,
+                                       int minMinutes,
+                                       int maxMinutes,
+                                       int minCost,
+                                       int maxCost,
+                                       boolean isOutside);
 }
