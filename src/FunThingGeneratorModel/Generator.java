dddcb79db426e857ada04cba55650387ff255374
lbreite <lbreite@ccs.neu.edu>
2016-10-01 20:20:06 -0400

    Generator done?

--- src/FunThingGeneratorModel/Generator.java
+++ src/FunThingGeneratorModel/Generator.java
@@ -7,20 +7,34 @@ import java.util.Random;
  * Generate activity suggestions.
  */
 public class Generator  {
+
+    /**
+     *
+     * @param participants      the number of participants
+     * @param maxMinutes        the maximum time
+     * @param maxCost           the maximum cost
+     * @param isOutside         is it outside?
+     * @param categories        the possible categories
+     * @return                  a random fun thing from the categories given
+     * @throws NoMatchException if the list of categories is empty
+     */
     public static IFunThing generate(int participants,
                               int maxMinutes,
                               int maxCost,
                               boolean isOutside,
                               List<Category> categories) throws NoMatchException {
 
+        //if the list is empty (it should never be able to be empty)
         if (categories.isEmpty()) {
             throw new NoMatchException("YOU FAILED. ITS NOT EVEN POSSIBLE. HOW?????");
         }
 
+        //make a random number to select a random category
         Random random = new Random();
         int randomInt = random.nextInt(categories.size());
         Category c = categories.get(randomInt);
 
+        //create the appropriate fun thing
         switch (c) {
             case MOVIE :
                 return new Movie(participants, maxMinutes, maxCost, isOutside);
@@ -32,6 +46,7 @@ public class Generator  {
                 return new Restaurant(participants, maxMinutes, maxCost, isOutside);
         }
 
+        //you should never get here
         return null;
     }
 }
