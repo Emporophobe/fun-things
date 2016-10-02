cfebadd6f606292317cebbd313b468b58146053d
Michael <emporophobe@gmail.com>
2016-10-01 20:51:31 -0400

    Finished Restaurant selection and interface methods.

--- src/FunThingGeneratorModel/Restaurant.java
+++ src/FunThingGeneratorModel/Restaurant.java
@@ -1,6 +1,8 @@
 package FunThingGeneratorModel;
 
 import javafx.util.Pair;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -8,24 +10,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Query the Zomato API to get restaurants.
  */
 public class Restaurant extends AbstractFunThing {
 
+    private JSONObject chosenRestaurant;
+
     Restaurant(int participants, int maxMinutes, int maxCost, boolean isOutside) throws NoMatchException {
         super(participants, maxMinutes, maxCost, isOutside);
     }
 
-    public static void main(String[] args) {
-        try {
-            Restaurant foo = new Restaurant(0, 0, 0, false);
-            foo.generate(0, 0, 0, false);
-        } catch (NoMatchException e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) {
+//        try {
+//            Restaurant foo = new Restaurant(0, 0, 100, false);
+//            //foo.generate(0, 0, 0, false);
+//            System.out.println(foo.getName());
+//            System.out.println(foo.getInfoString());
+//        } catch (NoMatchException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     /**
      * Get the latitude/longitude of the user
@@ -46,7 +55,7 @@ public class Restaurant extends AbstractFunThing {
         Pair<Double, Double> coords = getCoords();
 
         String zomatoApiBaseUrl = "https://developers.zomato.com/api/v2.1/";
-        String query = String.format("search?lat=%f&lon=%f&radius=%d", coords.getKey(), coords.getValue(), 1000);
+        String query = String.format("search?count=100&lat=%f&lon=%f&radius=%d", coords.getKey(), coords.getValue(), 1000);
         String url = zomatoApiBaseUrl + query;
 
         try {
@@ -64,7 +73,12 @@ public class Restaurant extends AbstractFunThing {
             }
             in.close();
 
-            JSONObject json = new JSONObject(response);
+            JSONObject json = null;
+            try {
+                json = new JSONObject(response.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
             return json;
 
@@ -78,16 +92,47 @@ public class Restaurant extends AbstractFunThing {
     @Override
     void generate(int participants, int maxMinutes, int maxCost, boolean isOutside) throws NoMatchException {
         JSONObject json = httpRequest();
-
+        try {
+            JSONArray restaurants = json.getJSONArray("restaurants");
+            List<JSONObject> matches = new ArrayList<>();
+            for (int i = 0; i < restaurants.length(); i++) {
+                JSONObject restaurant = (JSONObject) restaurants.get(i);
+                JSONObject rest = (JSONObject) restaurant.get("restaurant");
+                if (rest.getInt("average_cost_for_two") / 2 <= maxCost) {
+                    matches.add(rest);
+                }
+            }
+            if (matches.size() > 0) {
+                chosenRestaurant = matches.get(new Random().nextInt(matches.size()));
+            } else {
+                throw new NoMatchException("No matching restaurants");
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String getName() {
-        return null;
+        try {
+            return chosenRestaurant.getString("name");
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return "NO NAME";
+        }
     }
 
     @Override
     public String getInfoString() {
-        return null;
+        try {
+            String sb = chosenRestaurant.get("cuisines") +
+                    "\n\n" +
+                    ((JSONObject) chosenRestaurant.get("location")).get("address");
+
+            return sb;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return "NO INFO";
+        }
     }
 }
